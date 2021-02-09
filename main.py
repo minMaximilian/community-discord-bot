d567dd2ec7ccb9696e47d9f602d109dbca313d2c
max <sebastian.8295.8295@gmail.com>
2021-02-09 22:15:08 +0000

    added a startup function to create server objects

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import discord
 from discord.ext import commands
 import host_commands
 
+import json
+
 TOKEN = os.getenv('DISCORD_TOKEN')
 PREFIX = os.getenv('DISCORD_PREFIX')
 
@@ -11,6 +13,28 @@ if not(PREFIX):
     PREFIX="?"
 
 bot = commands.Bot(command_prefix=commands.when_mentioned_or(PREFIX))
+
+
+@bot.event
+async def on_ready():
+    await start_up()
+
+# Ensures that each server has a json file for storage
+# Using json files due to this being a private bot and scalability isn't a factor
+# Upgrade to mongodb eventually
+async def start_up():
+    for guild in bot.guilds:
+        file_name = str(guild.id) + ".json"
+        
+        data = {
+            guild.id: {
+                'registry': {}
+            }
+        }
+
+        if not(os.path.isfile(file_name)):
+            with open(file_name, "w") as f:
+                json.dump(data, f)
 
 # Adds different command modules to the bot
 bot.add_cog(host_commands.Host(bot))
