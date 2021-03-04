32a2f1bf45f706efd6af952e782e7786112dd837
max <sebastian.8295.8295@gmail.com>
2021-03-04 14:30:57 +0000

    scheduling

--- scheduling.py
+++ scheduling.py
@@ -2,15 +2,20 @@ from databaseClient import scheduler
 from main import bot
 import datetime
 import time
+from databaseClient import serversDB
 
 def schedule(schedule_obj, game, guild_id):
     announce_in = schedule_obj['datetime'].timestamp() - time.time()
-    scheduler.enter(announce_in, 1, makeAnnouncement(guild_id, game))
+    scheduler.enter(announce_in, 1, makeAnnouncement(guild_id, game, schedule_obj['repeat']))
 
-def re_schedule():
+def re_schedule(guild_id, game):
     pass
 
-def makeAnnouncement(guild_id, game):
+def makeAnnouncement(guild_id, game, repeat):
     guild = bot.get_guild(guild_id)
-    guild.get_channel()
-    
+    obj = serversDB.find_one({guild_id: {'$exists': True}})
+    channel = guild.get_channel(obj[game]['channel'])
+    channel.send(f'Game of {game} is starting now')
+    if repeat:
+        serversDB.update_one({guild_id: {'$exists': True}}, {guild_id: {game: {'schedule'}}})
+        re_schedule(guild_id, game)
